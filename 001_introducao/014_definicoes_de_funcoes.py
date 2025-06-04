c55a250107e72f9cdd182dab7fcafe5d784b68ac
Everton William <evertonwilliam@gmail.com>
2025-06-04 16:42:05 -0300

    Introdução a Funções

---
+++ 001_introducao/014_definicoes_de_funcoes.py
@@ -0,0 +1,19 @@
+'''
+Programa para definir funções em Python
+
+Def é uma palavra-chave em Python usada para definir funções.
+Funções são blocos de código reutilizáveis que realizam uma tarefa específica.
+Elas podem receber parâmetros e retornar valores.
+Aqui está um exemplo simples de uma função que exibe uma saudação personalizada.
+
+Desenvolvido por: Everton William Constantino
+Data: 04/06/2025
+'''
+
+#Definição de função
+def saudacao(nome):
+    """Exibe uma saudação personalizada."""
+    print(f"Olá, {nome}! Bem-vindo ao nosso programa.")
+
+# Chamada da função
+saudacao("Everton William Constantino")
